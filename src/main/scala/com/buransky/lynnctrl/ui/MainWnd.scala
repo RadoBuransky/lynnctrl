f61abc27c66ab1369fcf5a261e06d1d9b1ac00ce
Rado Buransky <radoburansky@gmail.com>
2020-01-05 14:49:10 -0800

    Main window with a single button

--- src/main/scala/com/buransky/lynnctrl/ui/MainWnd.scala
+++ src/main/scala/com/buransky/lynnctrl/ui/MainWnd.scala
@@ -2,14 +2,19 @@ package com.buransky.lynnctrl.ui
 
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
+
 import scala.swing._
 
 object MainWnd {
   def apply(): Behavior[Nothing] =
     Behaviors.setup[Nothing] { context =>
-      val frame = new MainFrame()
-      frame.maximize()
-      frame.open()
+      val frame = new MainFrame {
+        contents = new FlowPanel {
+          contents += new Button("Whatever")
+        }
+        maximize()
+        open()
+      }
 
       new MainWnd(context)
     }
