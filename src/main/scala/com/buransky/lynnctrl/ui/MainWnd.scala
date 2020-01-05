c6129cccd31d64ab02c7c13b977db56cdcc23e24
Rado Buransky <radoburansky@gmail.com>
2020-01-05 15:35:57 -0800

    Main menu bar

--- src/main/scala/com/buransky/lynnctrl/ui/MainWnd.scala
+++ src/main/scala/com/buransky/lynnctrl/ui/MainWnd.scala
@@ -8,18 +8,30 @@ import scala.swing._
 object MainWnd {
   def apply(): Behavior[Nothing] =
     Behaviors.setup[Nothing] { context =>
-      val frame = new MainFrame {
-        contents = new FlowPanel {
-          contents += new Button("Whatever")
-        }
-        maximize()
-        open()
-      }
-
-      new MainWnd(context)
+      val frame = initFrame()
+      new MainWnd(frame, context)
     }
+
+  private def initFrame(): Frame = {
+    new MainFrame {
+      title = "Lynn Controller"
+      menuBar = initMenuBar()
+      maximize()
+      open()
+    }
+  }
+
+  private def initMenuBar(): MenuBar = {
+    new MenuBar {
+      contents += new Menu("File") {
+        contents += new MenuItem(Action("Exit") {
+          System.exit(0)
+        })
+      }
+    }
+  }
 }
 
-class MainWnd(context: ActorContext[Nothing]) extends AbstractBehavior[Nothing](context) {
+class MainWnd(frame: Frame, context: ActorContext[Nothing]) extends AbstractBehavior[Nothing](context) {
   override def onMessage(msg: Nothing): Behavior[Nothing] = Behaviors.unhandled
 }
