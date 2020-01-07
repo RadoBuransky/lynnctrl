2b69b663f41e08ee03a0fc02a3b35e3676146f7b
Rado Buransky <radoburansky@gmail.com>
2020-01-06 18:00:28 -0800

    Separate VLC video and audio players are working

--- src/main/scala/com/buransky/lynnctrl/media/VlcPlayer.scala
+++ src/main/scala/com/buransky/lynnctrl/media/VlcPlayer.scala
@@ -2,22 +2,50 @@ package com.buransky.lynnctrl.media
 
 import java.io.File
 
+import com.buransky.lynnctrl.util.Logging
 import javax.swing.JFrame
 import uk.co.caprica.vlcj.factory.MediaPlayerFactory
+import uk.co.caprica.vlcj.media.{Media, MediaEventAdapter, MediaParsedStatus}
+import uk.co.caprica.vlcj.player.component.{AudioPlayerComponent, EmbeddedMediaPlayerComponent}
 
-class VlcPlayer extends AutoCloseable {
-  private val factory = new MediaPlayerFactory()
-  private val mediaPlayer = factory.mediaPlayers().newMediaPlayer()
+class VlcPlayer extends AutoCloseable with Logging {
+  private val videoPlayer = new EmbeddedMediaPlayerComponent()
+  private val audioPlayer = new AudioPlayerComponent()
 
   def play(frame: JFrame): Unit = {
     val audioFilePath = new File("""c:\Users\rado\OneDrive\Documents\Cubase Projects\2019-12-07 Pri sklenom dube\Audio\01_PriSklenomDube.wav""")
     val videoFilePath = new File("""c:\Users\rado\Downloads\SampleVideo_1280x720_10mb.mp4""")
 
-    mediaPlayer.media().play(audioFilePath.getAbsolutePath)
+    frame.setContentPane(videoPlayer)
+    frame.setVisible(true)
+    videoPlayer.mediaPlayer().media().prepare(videoFilePath.getAbsolutePath)
+    audioPlayer.mediaPlayer().media().prepare(encodeFileToVLCString(audioFilePath))
+
+    videoPlayer.mediaPlayer().media().events().addMediaEventListener(new MediaEventAdapter() {
+      override def mediaParsedChanged(media: Media, newStatus: MediaParsedStatus): Unit = {
+        if (MediaParsedStatus.DONE == newStatus) {
+          logger.info("Parsing done.")
+          videoPlayer.mediaPlayerFactory().submit(new Runnable {
+            override def run(): Unit = {
+              videoPlayer.mediaPlayer().audio().setTrack(-1)
+            }
+          })
+        }
+      }
+    })
+
+    videoPlayer.mediaPlayer().media().parsing().parse()
+    audioPlayer.mediaPlayer().media().parsing().parse()
+
+    videoPlayer.mediaPlayer().controls().play()
+    audioPlayer.mediaPlayer().controls().play()
   }
 
   override def close(): Unit = {
-    mediaPlayer.release()
-    factory.release()
+    audioPlayer.release()
+    videoPlayer.release()
   }
+
+  private def encodeFileToVLCString(filename: File): String =
+    filename.toURI.toASCIIString.replaceFirst("file:/", "file:///")
 }
